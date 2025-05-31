54d10fe63689fc6abf26e5a7bf1f14bff0696735
Bonjov Hima <bonjovhima@hotmail.de>
2025-05-31 21:18:14 +0200

    set up project with tabs for different screens

---
+++ styles/auth.styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+// const { width, height } = Dimensions.get("window");
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 50,
+  },
+  });
