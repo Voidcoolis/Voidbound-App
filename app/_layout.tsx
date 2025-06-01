365fa7be5a8ed0ef5ed5cd6187fd9d301f1a4df5
Bonjov Hima <bonjovhima@hotmail.de>
2025-06-01 14:56:08 +0200

    setup convex database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,16 @@
-import { tokenCache } from '@/cache';
-import InitialLayout from '@/components/initialLayout';
-import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
+import InitialLayout from "@/components/initialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 // this is the first file that is run. The Root Layout
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!publishableKey) {
-  throw new Error(
-    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
-  );
-}
-
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <SafeAreaProvider>
-          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-            <InitialLayout />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// VITE_CLERK_FRONTEND_API_URL= https://noble-hare-31.clerk.accounts.dev
+
+export default {
+  providers: [
+    {
+      domain: "https://noble-hare-31.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
